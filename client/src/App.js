f93d7afe6988b05fa87dde71e39ef0240eb65643
CamiloTello002 <camilojtello5@gmail.com>
2024-04-18 15:15:22 -0500

    added a post sample - It includes an image, a h2 title and a paragraph, where the main content goes - The post is about Theprimeagen haha.

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,19 @@ function App() {
           <a href="">Register</a>
         </nav>
       </header>
+
+      <div class="post">
+        <img
+          src="https://pbs.twimg.com/profile_images/1759330620160049152/2i_wkOoK_400x400.jpg"
+          alt=""
+        />
+        <h2>theprimeagen is a good programmer</h2>
+        <p>
+          Theprimeagen is a programmer from the USA, he is about 37 years old
+          and he has a good understanding of Neovim and plugins. He's also a fan
+          of Rust and Lua languages
+        </p>
+      </div>
     </main>
   );
 }
