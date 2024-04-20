54f80390e8b613256ea2750523945cca13a4a77f
CamiloTello002 <camilojtello5@gmail.com>
2024-04-20 12:20:32 -0500

    Bottom margin added Each post card needs to be separated a little bit, so a margin of 30px was added to accomplish this.

--- client/src/App.js
+++ client/src/App.js
@@ -29,6 +29,23 @@ function App() {
           </p>
         </div>
       </div>
+
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://pbs.twimg.com/profile_images/1759330620160049152/2i_wkOoK_400x400.jpg"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>theprimeagen is a good programmer</h2>
+          <p>
+            Theprimeagen is a programmer from the USA, he is about 37 years old
+            and he has a good understanding of Neovim and plugins. He's also a
+            fan of Rust and Lua languages
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
