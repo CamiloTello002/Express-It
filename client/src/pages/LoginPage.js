970317766e05ffdb5e4f7657b87bda7ddf12566b
CamiloTello002 <camilojtello5@gmail.com>
2024-04-20 17:19:55 -0500

    Added a title and modified button - Both the register and the login page now have their own title so that we can distinguish both. - The button is rounded, has some padding at the top and bottom, gray background and white font

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -2,6 +2,7 @@ export default function LoginPage() {
   //   return <div>Login Page</div>;
   return (
     <form action="" className="login">
+      <h1>Login</h1>
       <input type="text" placeholder="username" />
       <input type="password" placeholder="······" />
       <button>Login</button>
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -2,6 +2,7 @@ export default function RegisterPage() {
   //   return <div>Login Page</div>;
   return (
     <form action="" className="register">
+      <h1>Register</h1>
       <input type="text" placeholder="username" />
       <input type="password" placeholder="······" />
       <button>Register</button>
