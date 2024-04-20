540b794a083fa016e9152af5fcbf68b8bd0d2327
CamiloTello002 <camilojtello5@gmail.com>
2024-04-20 12:35:39 -0500

    Author and time added Every post need their author and the time it was uploaded, so that's why we added a link to the author and a time element for the upload date

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,12 @@ function App() {
         </div>
         <div className="texts">
           <h2>theprimeagen is a good programmer</h2>
+          <p className="info">
+            <a href="#" className="author">
+              Camilo Rueda
+            </a>
+            <time>2024-04-20 16:45</time>
+          </p>
           <p>
             Theprimeagen is a programmer from the USA, he is about 37 years old
             and he has a good understanding of Neovim and plugins. He's also a
