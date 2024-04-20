9a0746830ce37af168dffe43eb6610bfd57a2518
CamiloTello002 <camilojtello5@gmail.com>
2024-04-20 12:44:15 -0500

    Edited info paragraph The information paragraph has the following: - a gray-ish color so that it makes a distinction. - font size is conserved the same as the root. - We want to make it bold. - A little of margin at the top and bottom, no margin at the sides.

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,7 @@ function App() {
             </a>
             <time>2024-04-20 16:45</time>
           </p>
-          <p>
+          <p className="summary">
             Theprimeagen is a programmer from the USA, he is about 37 years old
             and he has a good understanding of Neovim and plugins. He's also a
             fan of Rust and Lua languages
