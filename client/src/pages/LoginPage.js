e9158ad0d357b01d483812025841773402229d96
CamiloTello002 <camilojtello5@gmail.com>
2024-04-20 16:55:05 -0500

    modified style for form and input elements - The form element will extend themselves no more than 400 px and they are centered because the left and right margin are set automatically, which means it'll be evenly spaced. - The input elements are displayed as blocks so that each input element has their own line, not sharing it with any other element.

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,7 +1,7 @@
 export default function LoginPage() {
   //   return <div>Login Page</div>;
   return (
-    <form action="">
+    <form action="" className="login">
       <input type="text" placeholder="username" />
       <input type="password" placeholder="······" />
       <button>Login</button>
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,7 +1,7 @@
 export default function RegisterPage() {
   //   return <div>Login Page</div>;
   return (
-    <form action="">
+    <form action="" className="register">
       <input type="text" placeholder="username" />
       <input type="password" placeholder="······" />
       <button>Register</button>
