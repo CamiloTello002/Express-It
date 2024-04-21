5f4265438f6c4a3f83b8d22e6cbea67f1ebaa233
CamiloTello002 <camilojtello5@gmail.com>
2024-04-21 13:08:59 -0500

    added an event handler when the user enters password or username

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,10 +1,26 @@
+import { useState } from 'react';
 export default function RegisterPage() {
-  //   return <div>Login Page</div>;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  function register(ev) {
+    ev.preventDefault();
+  }
   return (
-    <form action="" className="register">
+    <form onSubmit={register} className="register">
       <h1>Register</h1>
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="······" />
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(ev) => setUsername(ev.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(ev) => setPassword(ev.target.value)}
+      />
+      {/* <input type="password" placeholder="······" /> */}
       <button>Register</button>
     </form>
   );
