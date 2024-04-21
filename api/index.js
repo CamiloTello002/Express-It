c164f510b0ec9968be47faef7d2a92dbbcbab2d9
CamiloTello002 <camilojtello5@gmail.com>
2024-04-21 15:33:44 -0500

    Added CORS CORS had to be enabled so that the React application can make requests to it (though our react app is in the same domain, it's in a different port, and that different port counts as another origin.

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+
 app.post('/', (req, res) => {
-  res.json('test ok');
+  console.log(req.body);
+  res.status(200).json('test ok');
 });
 
 app.listen(4000, () => {
