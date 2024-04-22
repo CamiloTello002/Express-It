58fcb07d097c6db989d193babc4890df1ed7e646
CamiloTello002 <camilojtello5@gmail.com>
2024-04-22 14:43:24 -0500

    password verification now works - First, the api checks if the user has entered some username or password. - Then, the api checks if the user exists. In case it does, then verifies if the password matches. If none is met, then a 400 status code is returned - In case it all matches, then a 200 status code is returned.

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: './.env' });
 
 const app = express();
@@ -39,13 +40,59 @@ app.post('/register', async (req, res) => {
   }
 });
 
-app.post('/login', async (req, res) => {
+app.post('/login', async (req, res, next) => {
   // 1) get username and password from body
   const { username, password } = req.body;
+  // if there's not username nor password, throw an error
+  if (!username || !password) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'You need to enter a username or password',
+    });
+  }
   try {
-    const userDoc = await User.findOne({ username });
-    res.status(200).json(userDoc);
+    // get the document (please, AWAIT it)
+    const userDoc = await User.findOne({ username }).select('+password');
+
+    // in case there's no user or password is incorrect, return 400 status code
+    if (!userDoc || !(await bcrypt.compare(password, userDoc.password))) {
+      return res.status(400).json({
+        status: 'failed',
+        message: 'username or password incorrect!',
+      });
+    }
+    jwt.sign();
+    return res.status(200).json({});
   } catch (error) {}
+  // try {
+  //   // 2) make the request to the database (really, we're using the await keyword)
+  //   const userDoc = await User.findOne({ username });
+  //   if (!userDoc) {
+  //     return res.status(400).json({
+  //       status: 'failed',
+  //       message: 'username or password incorrect',
+  //     });
+  //   }
+  //   console.log(userDoc);
+  //   // 3) check if the password matches
+  //   const passwordIsCorrect = await bcrypt.compare(password, userDoc.password);
+  //   // res.status(200).json({ passwordIsCorrect });
+  //   if (passwordIsCorrect) {
+  //     res.status(200).json({
+  //       status: 'success',
+  //       message: 'Successfully logged in :)',
+  //       user: userDoc,
+  //     });
+  //   } else {
+  //     res.status(400).json({
+  //       status: 'failed',
+  //       message: 'Not logged in..',
+  //     });
+  //   }
+  // } catch (error) {
+  //   console.log(error);
+  //   res.status(400).send('some error happened');
+  // }
 });
 
 app.listen(port, () => {
