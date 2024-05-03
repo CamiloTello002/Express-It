37e49a274be629896443d47982f891e6ee999fed
CamiloTello002 <camilojtello5@gmail.com>
2024-05-03 15:40:08 -0500

    added credentials option so that we can save credentials sent by the server

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -16,6 +16,7 @@ export default function LoginPage() {
       method: 'POST',
       body: JSON.stringify({ username, password }),
       headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
     });
     alert(`Login ${response.ok ? 'successful :)' : 'failed :('}`);
   }
