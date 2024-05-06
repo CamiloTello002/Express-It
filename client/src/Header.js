7929fa2967488b2ef45182786effd34b69d911a6
CamiloTello002 <camilojtello5@gmail.com>
2024-05-06 16:21:05 -0500

    Header component will no longer make multiple requests to the server

--- client/src/Header.js
+++ client/src/Header.js
@@ -2,22 +2,12 @@ import { UserContext } from 'UserContext';
 import { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 export default function Header() {
-  // we'll no longer use it since the context provides this information
-  // const [username, setUsername] = useState(null);
-  // taking information about the user with useContext
   const { setUserInfo, userInfo } = useContext(UserContext);
   useEffect(() => {
-    // our fetch function will take the response with then()
     fetch('http://localhost:4000/profile', {
       credentials: 'include',
     }).then((response) => {
-      // response parsed to json
-      response.json().then((userInfo) => {
-        // Set the username
-        // this setUsername won't come from useState but from useContext
-        // setUsername(userInfo.username);
-        setUserInfo(userInfo);
-      });
+      console.log(response);
     });
   });
 
