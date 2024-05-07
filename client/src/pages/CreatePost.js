3d9d65e5ab9afc1efc422a927f706257bffe1efb
CamiloTello002 <camilojtello5@gmail.com>
2024-05-07 10:09:09 -0500

    CreatePost component added

---
+++ client/src/pages/CreatePost.js
@@ -0,0 +1,5 @@
+// with doing export default function, we can smell a component about to be created
+export default function CreatePost() {
+  // return a simple div for the sake of completeness
+  return <div>Here you'll create your new post</div>;
+}
