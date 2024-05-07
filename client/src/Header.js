63bd3076ffe7f595275c3d154f4da3392eb089cf
CamiloTello002 <camilojtello5@gmail.com>
2024-05-07 09:06:51 -0500

    Header component changed - It now safely makes the request to the API - Previously, it would make multiple requests when not using the cleanup function and using callbacks. - In this case, we use async await (promises) and set a cleanup function.

--- client/src/Header.js
+++ client/src/Header.js
@@ -2,19 +2,28 @@ import { UserContext } from 'UserContext';
 import { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 export default function Header() {
+  // user information provided by a parent component
   const { setUserInfo, userInfo } = useContext(UserContext);
+  // set a side effect when rendering the component
   useEffect(() => {
+    // instace of abort controller
     const controller = new AbortController();
+    // function for fetching data from API
     const fetchData = async () => {
       try {
+        // make the request to the API and save the response
         const response = await fetch('http://localhost:4000/profile', {
           credentials: 'include',
           signal: controller.signal,
         });
+        // json-ize the response
         const json = await response.json();
-        console.log(json);
+        // extract the username from the response and set it to the userInfo state
+        setUserInfo(json.username);
       } catch (err) {}
     };
+    // run the function that makes the request to the API and
+    // changes the userInfo state
     fetchData();
 
     // return the cleanup function
@@ -32,7 +41,9 @@ export default function Header() {
     setUserInfo(null);
   }
 
-  const username = userInfo?.username;
+  const username = userInfo;
+  console.log('the username is...');
+  console.log(username);
 
   return (
     <header>
