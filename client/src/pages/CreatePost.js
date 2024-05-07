a0d3e97e3645781274e05d16ffbac3a638735013
CamiloTello002 <camilojtello5@gmail.com>
2024-05-07 10:58:03 -0500

    Added necessary props for the components inside the form

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -3,14 +3,64 @@ import { useState } from 'react';
 import 'react-quill/dist/quill.snow.css';
 // with doing export default function, we can smell a component about to be created
 export default function CreatePost() {
-  const [value, setValue] = useState('');
+  const [title, setTitle] = useState('');
+  const [summary, setSummary] = useState('');
+  const [content, setContent] = useState('');
+
+  // include modules (for customization)
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [
+        { list: 'ordered' },
+        { list: 'bullet' },
+        { indent: '-1' },
+        { indent: '+1' },
+      ],
+      ['link', 'image'],
+      ['clean'],
+    ],
+  };
+
+  // include formats (for customization)
+  const formats = [
+    'header',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'indent',
+    'link',
+    'image',
+  ];
+
+  // finally return the rendered component
   return (
     <form>
-      <input type="title" placeholder={'Title'} />
-      <input type="summary" placeholder={'Summary'} />
+      <input
+        type="title"
+        placeholder={'Title'}
+        value={title}
+        onChange={(ev) => setTitle(ev.target.value)}
+      />
+      <input
+        type="summary"
+        placeholder={'Summary'}
+        value={summary}
+        onChange={(ev) => setSummary(ev.target.value)}
+      />
       <input type="file" />
       {/* <textarea name="" id=""></textarea> */}
-      <ReactQuill />
+      <ReactQuill
+        value={content}
+        modules={modules}
+        formats={formats}
+        onChange={(newValue) => setContent(newValue)}
+      />
       <button style={{ marginTop: '7px' }}>Create post</button>
     </form>
   );
