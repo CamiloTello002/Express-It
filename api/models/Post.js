738ce662342bc5a944d87526320949b3351e502e
CamiloTello002 <camilojtello5@gmail.com>
2024-05-09 15:21:58 -0500

    Post model created

--- api/models/Post.js
+++ api/models/Post.js
@@ -1,4 +1,21 @@
 const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
-const PostSchema = new Schema();
+const PostSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'You must provide a string'],
+    },
+    summary: String,
+    content: String,
+    cover: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const PostModel = model('Post', PostSchema);
+
+module.exports = PostModel;
