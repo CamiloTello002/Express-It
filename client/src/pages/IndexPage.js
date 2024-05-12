689f38c8713b4dcfdc2fb387fb6c5c131288a117
CamiloTello002 <camilojtello5@gmail.com>
2024-05-11 19:51:06 -0500

    Index page requests posts from the server

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -1,6 +1,23 @@
+import { useEffect } from 'react';
 import Post from '../Post';
 
 export default function IndexPage() {
+  // fetch posts from server
+  useEffect(() => {
+    const controller = new AbortController();
+
+    const fetchData = async () => {
+      try {
+        const posts = await fetch('/posts', {
+          credentials: 'include',
+          signal: controller.signal,
+          method: 'GET',
+        });
+        const postsJSON = await posts.json();
+        console.log(postsJSON);
+      } catch (error) {}
+    };
+  }, []);
   return (
     <>
       <Post />
