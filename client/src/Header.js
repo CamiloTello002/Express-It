c44c19a212b9a6bc96d8be98fab03a67a0fcd96f
CamiloTello002 <camilojtello5@gmail.com>
2024-05-11 19:50:38 -0500

    Comments improved to explain why it was needed

--- client/src/Header.js
+++ client/src/Header.js
@@ -8,39 +8,34 @@ export default function Header() {
   const pathLogout = '/logout';
   const URLToAPI = new URL(path, baseURL);
   const URLToAPILogout = new URL(pathLogout, baseURL);
-  // user information provided by a parent component
   const { setUserInfo, userInfo } = useContext(UserContext);
-  // set a side effect when rendering the component
+
   useEffect(() => {
-    // instace of abort controller
+    // Fetch has to be ignored in case it's unmounted
     const controller = new AbortController();
-    // function for fetching data from API
     const fetchData = async () => {
       try {
-        // make the request to the API and save the response
+        // User data is in the SERVER
         const response = await fetch(URLToAPI, {
           credentials: 'include',
           signal: controller.signal,
         });
-        // json-ize the response
+        // Useful for setting user information
         const json = await response.json();
         console.log('the response from /profile is');
         console.log(json);
-        // Don't just set the username; set the ENTIRE object
         setUserInfo(json);
       } catch (err) {}
     };
-    // run the function that makes the request to the API and
-    // changes the userInfo state
+    // useEffect DOESN'T allow async functions as first parameter,
+    // so a separate function was needed
     fetchData();
 
-    // return the cleanup function
     return () => {
       controller.abort();
     };
-  }, []); // empty dependency array means effect runs only once
+  }, []);
 
-  // What the logout function will do is to invalidate the cookie.
   function logout() {
     fetch(URLToAPILogout, {
       credentials: 'include',
@@ -49,8 +44,7 @@ export default function Header() {
     setUserInfo(null);
   }
 
-  // since userInfo might be empty (because of server response),
-  // we safely access the username
+  // userInfo might be returned as empty object
   const username = userInfo?.username;
   console.log('the username is...');
   console.log(username);
