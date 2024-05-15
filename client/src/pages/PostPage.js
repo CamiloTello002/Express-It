93957877fe22ea4dc7a9159d461c2e6200e37953
CamiloTello002 <camilojtello5@gmail.com>
2024-05-15 10:23:07 -0500

    Post page now takes url parameter

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,3 +1,13 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 export default function PostPage() {
+  const { id } = useParams();
+  console.log(id);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await fetch(`http://localhost:4000/post/${id}`);
+  //   };
+  // });
   return <div>this post page works</div>;
 }
