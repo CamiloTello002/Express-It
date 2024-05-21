0a5b0e43cf741308e8da7b0e5c1b86e7515abd57
CamiloTello002 <camilojtello5@gmail.com>
2024-05-21 15:17:14 -0500

    when in development, frontend gets information from the local api

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,6 @@
 let baseURL;
 if (process.env.REACT_APP_NODE_ENV === 'production')
   baseURL = 'https://blog-api-ddt8.onrender.com';
-if (process.env.REACT_APP_NODE_ENV === 'development')
-  baseURL = 'http://localhost:4000';
+else baseURL = 'http://localhost:4000';
 
 export { baseURL };
