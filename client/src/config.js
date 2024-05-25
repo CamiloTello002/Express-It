9d76af7d27d799b3ff7e89b329bc9b8d9a3c66f8
CamiloTello002 <camilojtello5@gmail.com>
2024-05-25 13:01:39 -0500

    quick fix

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,3 @@
-let baseURL;
-if (process.env.REACT_APP_NODE_ENV === 'production')
-  baseURL = 'https://blog-api-ddt8.onrender.com';
-else baseURL = 'http://localhost:4000';
+baseURL = 'https://blog-api-ddt8.onrender.com';
 
 export { baseURL };
