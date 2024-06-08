fa67c4008aea64ac19b03745a202d45cf9ce21ad
CamiloTello002 <camilojtello5@gmail.com>
2024-06-08 16:57:50 -0500

    feat: added login controller

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -14,3 +14,40 @@ exports.register = async (req, res) => {
         res.status(400).json(error.errorResponse.errmsg);
     }
 }
+
+exports.login = async (req, res, next) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return res.status(404).json({
+            status: 'failed',
+            message: 'You need to enter a username or password',
+        });
+    }
+    try {
+        const userDoc = await User.findOne({ username }).select('+password');
+
+        if (!userDoc || !(await bcrypt.compare(password, userDoc.password))) {
+            return res.status(400).json({
+                status: 'failed',
+                message: 'username or password incorrect!',
+            });
+        }
+        jwt.sign(
+            { username, id: userDoc._id },
+            process.env.JWT_SECRET,
+            {},
+            (err, token) => {
+                if (err) throw err;
+                res
+                    .cookie('token', token, {
+                        secure: true,
+                        sameSite: 'none',
+                    })
+                    .json({
+                        id: userDoc._id,
+                        username,
+                    });
+            }
+        );
+    } catch (error) { }
+}
