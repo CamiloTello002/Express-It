f8682cd0d2e41cf7b69a5a3309c03beb0f3fa8d0
CamiloTello002 <camilojtello5@gmail.com>
2024-06-11 17:43:42 -0500

    all routes tested with postman

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -24,25 +24,28 @@ exports.getPosts = async (req, res) => {
 }
 
 exports.createPost = async (req, res, next) => {
-    // FIXME: validate these inputs
     const { title, summary, content } = req.body;
     const author = req.user._id;
+
     if (!title || !summary || !content)
         return next(new AppError('You must provie a title, summary and content', 401))
+
     const postParams = {
         title,
         summary,
         content,
         author
     };
-    // User may not upload a photo
+
     if (req.file !== undefined) {
         const { originalname } = req.file;
         postParams.cover = originalname;
     } else {
         postParams.cover = 'default.png';
     }
+
     const postDoc = await Post.create(postParams);
+
     res.status(200).json({
         status: 'success',
         message: 'Post created',
@@ -53,34 +56,29 @@ exports.createPost = async (req, res, next) => {
 }
 
 exports.getPost = async (req, res) => {
-    const { token } = req.cookies;
     const { id } = req.params; // extract id from parameter
     const response = {};
-    if (token) {
-        jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
-            if (err) throw err;
-            response.author = decodedToken.id;
-        });
-    }
+
     const post = await Post.findById(id).populate('author', 'username'); // query the post to the database
     response.status = 'success';
     response.message = 'Entire post page returned';
     response.post = post;
     res.status(200).json(response);
 }
-exports.updatePost = async (req, res) => {
+
+// TODO: add data validation and some error handling
+exports.updatePost = async (req, res, next) => {
     const { id } = req.params;
     const { title, summary, content } = req.body;
     let update = { title, summary, content };
     console.log(req.body.cover);
+
     if (req.body.cover) {
         update.cover = req.body.cover;
     }
-    // console.log(req.body);
-    // 1) find the document and update it
-    console.log('before updating, the update object looks like:');
-    console.log(update);
+
     await Post.findByIdAndUpdate(id, update);
+
     res.status(200).json({
         message: `PATCH /posts/${id} found!`,
     });
@@ -90,6 +88,5 @@ exports.deletePost = async (req, res) => {
     const { id } = req.params;
     // 2) delete it
     await Post.findByIdAndDelete(id);
-    console.log('route hit');
     res.status(204).send('done');
 }
--- api/routes/postRoutes.js
+++ api/routes/postRoutes.js
@@ -8,6 +8,6 @@ router.get('/', controller.getPosts);
 /** Routes only for logged in users */
 router.use(authController.protect);
 router.post('/create-post', controller.createPost);
-router.route('/post/:id').get(controller.getPost).patch(controller.uploadInstance.single('file'), controller.updatePost).delete(controller.deletePost);
+router.route('/:id').get(controller.getPost).patch(controller.uploadInstance.single('file'), controller.updatePost).delete(controller.deletePost);
 
 module.exports = router;
