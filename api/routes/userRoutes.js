c53369b9388f8bd9bbf1f70c555881a654eeb152
CamiloTello002 <camilojtello5@gmail.com>
2024-07-03 20:38:02 -0500

    Added documentation for login endpoint, which is pretty similar to register endpoint

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 /**
  * @openapi
- * /api/v1/register:
+ * /api/v1/users/register:
  *   post:
  *     summary: This endpoint allows a new user to register by providing a username and a password.
  *     tags:
@@ -56,6 +56,57 @@ const router = express.Router();
  *                                                  example: Berkley
  */
 router.post('/register', authController.register);
+/**
+ * @openapi
+ * /api/v1/users/login:
+ *   post:
+ *     summary: This endpoint allows an existing user to log in and start making posts.
+ *     tags:
+ *       - User
+ *     requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          username:
+ *                              type: string
+ *                              example: Berkley
+ *                          password:
+ *                              type: string
+ *                              format: password
+ *                              example: Astonishing
+ *                      required:
+ *                          - username
+ *                          - password
+ *     responses:
+ *          '201':
+ *              description: User created successfully
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              status:
+ *                                  type: string
+ *                                  example: success
+ *                              message:
+ *                                  type: string
+ *                                  example: Register successful!
+ *                              data:
+ *                                  type: object
+ *                                  properties:
+ *                                      user:
+ *                                          type: object
+ *                                          properties:
+ *                                              id:
+ *                                                  type: string
+ *                                                  example: 60c72b2f9b1d8e00215b78a0
+ *                                              username:
+ *                                                  type: string
+ *                                                  example: Berkley
+ */
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
 
