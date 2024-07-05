fdec87055ac02f49953dd0856a8a03db586750a7
CamiloTello002 <camilojtello5@gmail.com>
2024-07-04 23:42:46 -0500

    The URL-encoded data is set to be parsed with the querystring library

--- api/app.js
+++ api/app.js
@@ -22,7 +22,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static('uploads'));
 
