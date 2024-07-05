d723175da81a2893376c840c65131e87f703107c
CamiloTello002 <camilojtello5@gmail.com>
2024-07-05 06:39:39 -0500

    Post component was added to components folder

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import Post from '../Post';
+import Post from '../components/Post';
 import { baseURL } from 'config';
 
 export default function IndexPage() {
