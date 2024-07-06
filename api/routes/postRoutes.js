e2d7e32928f669821f216d26450d3d20ecc63802
CamiloTello002 <camilojtello5@gmail.com>
2024-07-05 22:46:39 -0500

    Added documentation for the endpoint that retrieves information about a single post

--- api/routes/postRoutes.js
+++ api/routes/postRoutes.js
@@ -27,6 +27,67 @@ const authController = require('./../controllers/authController')
  *                    type: array
  */
 router.get('/', controller.getPosts);
+/**
+ * @openapi
+ * /api/v1/posts/{id}:
+ *   get:
+ *     summary: Get a single post by ID
+ *     description: Retrieve a post along with its author's username.
+ *     tags:
+ *       - Posts
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        schema:
+ *          type: string
+ *        description: The ID of the post to retrieve
+ *     responses:
+ *       200:
+ *          description: Successful response with the post data
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          status: 
+ *                              type: string
+ *                              example: success
+ *                          message: 
+ *                              type: string
+ *                              example: Entire post page returned
+ *                          post:
+ *                              type: object
+ *                              properties:
+ *                                  _id:
+ *                                      type: string
+ *                                      example: 60b725f10c9d4b0014e49c74
+ *                                  title:
+ *                                      type: string
+ *                                      example: Post Title
+ *                                  content:
+ *                                      type: string
+ *                                      example: Post content goes here...
+ *                                  author:
+ *                                      type: object
+ *                                      properties:
+ *                                          username:
+ *                                              type: string
+ *                                              example: authorUsername
+ *       404:
+ *          description: Post not found
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          status:
+ *                              type: string
+ *                              example: error
+ *                          message:
+ *                              type: string
+ *                              example: Post not found
+ */
 router.get('/:id', controller.getPost);
 
 /** Routes only for logged in users */
