9ab66b68c8a981a8e8e23546afec5924b1cc23ef
CamiloTello002 <camilojtello5@gmail.com>
2024-07-11 10:43:27 -0500

    Added new backend uri

--- client/src/services/config.js
+++ client/src/services/config.js
@@ -1,4 +1,4 @@
-const baseURL = process.env.REACT_APP_API_BASE_URL || 'https://express-it.onrender.com';
+const baseURL = process.env.REACT_APP_API_BASE_URL || 'https://express-it-latest.onrender.com';
 console.log('the variable for the React application is')
 console.log(process.env.REACT_APP_API_BASE_URL);
 
